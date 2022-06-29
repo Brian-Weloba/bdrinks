ef0bcc598a3f98ef94a1a1c8d692ed4f19262784
Brian-Weloba <bweloba@gmail.com>
2022-06-29 12:55:51 +0300

    Change product button

--- src/components/Product.js
+++ src/components/Product.js
@@ -233,7 +233,7 @@ export default function Product({ products, loading }) {
                 <button
                   // to={`/product/${product.productId}`}
                   type="button"
-                  className=" inline-block px-6 py-2.5 bg-red-800 text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out text-base md:text-lg"
+                  className=" inline-block px-6 py-2.5 border-2 border-red-800 text-red-800 hover:text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-red-800  hover:shadow-lg focus:bg-red-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out text-base md:text-lg"
                 >
                   View
                 </button>
