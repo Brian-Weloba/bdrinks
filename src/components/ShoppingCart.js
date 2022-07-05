16ba05e49aa966198c051c5edce7aea0b7456cc7
Brian-Weloba <bweloba@gmail.com>
2022-07-05 19:14:09 +0300

    Empty cart check

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -9,8 +9,6 @@ export const ShoppingCart = () => {
   const [cookies] = useCookies(["cartItems"]);
   const [totalPrice, setTotalPrice] = useState(calculateTotalPrice());
   const [tax, setTax] = useState(totalPrice * 0.16);
-  
- 
 
   useEffect(() => {
     if (cookies.cartItems) {
@@ -31,37 +29,56 @@ export const ShoppingCart = () => {
     return total;
   }
 
-  return (
-    <div className="pt-16 grow bg-zinc-100">
-      <div className="sm:mx-4 md:mx-8 w-100 mx-0">
-        <h1 className="  text-red-800 font-bold text-center sm:text-left ml-6 text-2xl py-10  bg-transparent">
-          Shopping Cart
+  //if cart is empty, display message
+  if (cartItems.length === 0) {
+    return (
+      <div className=" pt-28 h-2/3 flex justify-center col-span-full ">
+        <h1>
+          Your cart is empty. <br />
+          <span className="text-red-700">
+            <a href="/">
+              <span className="text-red-700">
+                <u>Click here</u>
+              </span>
+            </a>
+          </span>{" "}
+          to continue shopping.
         </h1>
-        <div className="flex flex-col lg:flex-row">
-          <div className=" border-t-2 border-zinc-800 bg-transparent lg:w-3/5 m-2">
-            <div className="grid grid-cols-1">
-              <div className=" justify-center h-max">
-                <CartItem
-                  totalPrice={totalPrice}
-                  setTotalPrice={setTotalPrice}
-                  cartItems={cartItems}
-                  setCartItems={setCartItems}
-                  calculateTotalPrice={calculateTotalPrice}
-                />
+      </div>
+    );
+  } else {
+    return (
+      <div className="pt-16 grow bg-zinc-100">
+        <div className="sm:mx-4 md:mx-8 w-100 mx-0">
+          <h1 className="  text-red-800 font-bold text-center sm:text-left ml-6 text-2xl py-10  bg-transparent">
+            Shopping Cart
+          </h1>
+          <div className="flex flex-col lg:flex-row">
+            <div className=" border-t-2 border-zinc-800 bg-transparent lg:w-3/5 m-2">
+              <div className="grid grid-cols-1">
+                <div className=" justify-center h-max">
+                  <CartItem
+                    totalPrice={totalPrice}
+                    setTotalPrice={setTotalPrice}
+                    cartItems={cartItems}
+                    setCartItems={setCartItems}
+                    calculateTotalPrice={calculateTotalPrice}
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* <div className=" bg-sky-600 sm:w-2/5 m-2"> */}
-          <OrderSummary
-            tax={tax}
-            setTax={setTax}
-            totalPrice={totalPrice}
-            setTotalPrice={setTotalPrice}
-            cartItems={cartItems}
-          />
+            {/* <div className=" bg-sky-600 sm:w-2/5 m-2"> */}
+            <OrderSummary
+              tax={tax}
+              setTax={setTax}
+              totalPrice={totalPrice}
+              setTotalPrice={setTotalPrice}
+              cartItems={cartItems}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
