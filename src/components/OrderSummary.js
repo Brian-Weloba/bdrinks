d8cde1b1cb0c68cb173ecde6a0fde6ffe4905462
Brian-Weloba <bweloba@gmail.com>
2022-07-07 11:50:40 +0300

    Update message

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,26 +1,30 @@
 import { QuestionMarkCircleIcon } from "@heroicons/react/solid";
 import React from "react";
 
-export const OrderSummary = ({
-  totalPrice,
-  cartItems,
-}) => {
-
-  function whatsappCheckout(){
+export const OrderSummary = ({ totalPrice, cartItems }) => {
+  function whatsappCheckout() {
     let whatsappNumber = "254712000928";
-    let message = "Hello *Berny's*,%0a%0a I would like to order the following items:%0a%0a";
+    let message =
+      "Hello *Berny's*,%0a%0a I would like to order the following items:%0a%0a";
     cartItems.forEach((item) => {
-      message += `${item.productName} - ${item.quantity} x ${item.productPrice} = ${item.productPrice * item.quantity}%0a`;
+      let Subtotal = item.productPrice * item.quantity;
+      message += `${item.productName} ${item.productVolume} x ${
+        item.quantity
+      } = KES ${Subtotal.toString().replace(
+        /\B(?=(\d{3})+(?!\d))/g,
+        ","
+      )}%0a`;
     });
-    message += `%0aTotal: ${totalPrice}`;
-    message += "%0a Please deliver to: address. %0a%0a Thank you!";
+    message += `%0aTotal: KES ${totalPrice
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    message += "%0a%0a Please deliver to: address.%0a%0a Thank you!";
 
     window.open(
       `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${message}`,
       "_blank"
     );
-
-    }
+  }
 
   return (
     <div className="grid grid-cols-1 rounded-xl bg-zinc-200 lg:w-2/5 m-2 h-max">
@@ -57,11 +61,12 @@ export const OrderSummary = ({
           </h1>
         </div>
         <div className="grid grid-cols-1 m-4 ">
-          <button 
-          onClick={() => {
-            whatsappCheckout();
-          }}
-          className="bg-red-800 text-white font-bold py-2 px-4 rounded-lg">
+          <button
+            onClick={() => {
+              whatsappCheckout();
+            }}
+            className="bg-red-800 text-white font-bold py-2 px-4 rounded-lg"
+          >
             Checkout
           </button>
         </div>
